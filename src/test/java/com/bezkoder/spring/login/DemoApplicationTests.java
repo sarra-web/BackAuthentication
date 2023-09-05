6b9d829a12ca13e74d17d5032e6467a49250f1fe
sarra-web <sarra.jridette@etudiant-enit.utm.tn>
2023-09-05 07:52:30 +0100

    workflow2

--- src/test/java/com/bezkoder/spring/login/DemoApplicationTests.java
+++ src/test/java/com/bezkoder/spring/login/DemoApplicationTests.java
@@ -1,9 +1,13 @@
 package com.bezkoder.spring.login;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DemoApplicationTests {
 
 	@Test
